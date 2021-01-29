ac50f99330361296bb9e6a367b340bc0ea045aa1
evansmwenda <evansmwenda.em@gmail.com>
2021-01-29 10:40:51 +0300

    added simple gesture detector library

--- lib/screens/my_app.dart
+++ lib/screens/my_app.dart
@@ -1,35 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:swipedetector/swipedetector.dart';
+import 'package:simple_gesture_detector/simple_gesture_detector.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   static const routeName = '/';
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _text;
+
   bool isVisible = true;
   @override
+  void initState() {
+    super.initState();
+    _text = 'Swipe me!';
+  }
+
+  @override
   Widget build(BuildContext context) {
+    void _onVerticalSwipe(SwipeDirection direction) {
+      setState(() {
+        if (direction == SwipeDirection.up) {
+          _text = 'Swiped up!';
+          print('Swiped up!');
+        } else {
+          _text = 'Swiped down!';
+          print('Swiped down!');
+        }
+      });
+    }
+
+    void _onHorizontalSwipe(SwipeDirection direction) {
+      setState(() {
+        if (direction == SwipeDirection.left) {
+          _text = 'Swiped left!';
+          print('Swiped left!');
+        } else {
+          _text = 'Swiped right!';
+          print('Swiped right!');
+        }
+      });
+    }
+
+    void _onLongPress() {
+      setState(() {
+        _text = 'Long pressed!';
+        print('Long pressed!');
+      });
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text("NZK - 100"),
         leading: Icon(Icons.menu),
       ),
-      body: Container(
-        child:
-
-
-        SwipeDetector(
-          child: Center(child: Text("Hello ...songs here")),
-        onSwipeUp: () {
-          print("you have swipped up");
-        },
-        onSwipeDown: () {
-          print("you have swipped down");
-        },
-        onSwipeLeft: () {
-          print("you have swipped left");
-        },
-        onSwipeRight: () {
-          print("you have swipped right");
-        },
-      )
+      body: ListView(
+        children: [
+          Container(
+            height: 400,
+            width: double.infinity,
+            color: Colors.green,
+            margin: EdgeInsets.all(20),
+            child: SimpleGestureDetector(
+              onVerticalSwipe: _onVerticalSwipe,
+              onHorizontalSwipe: _onHorizontalSwipe,
+              onLongPress: _onLongPress,
+              swipeConfig: SimpleSwipeConfig(
+                verticalThreshold: 40.0,
+                horizontalThreshold: 40.0,
+                swipeDetectionBehavior:
+                    SwipeDetectionBehavior.continuousDistinct,
+              ),
+              child: _buildBox(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: Visibility(
         visible: isVisible,
@@ -37,6 +83,23 @@ class MyApp extends StatelessWidget {
           child: Icon(Icons.add),
           onPressed: () {},
           backgroundColor: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBox() {
+    return Container(
+      height: 160.0,
+      width: 160.0,
+      color: Colors.indigo,
+      child: Center(
+        child: Text(
+          _text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+          ),
         ),
       ),
     );
